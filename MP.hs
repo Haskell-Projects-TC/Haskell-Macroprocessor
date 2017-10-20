5d0d46769a0ff786785397fddcc9729d6e4fe499
Tiger Cross <tc2017@voxel05.doc.ic.ac.uk>
2017-10-20 14:01:17 +0100

    Implemented up to getKeywordDefs, need to add tests and comment more still

--- MP.hs
+++ MP.hs
@@ -12,11 +12,17 @@ separators :: String
 separators
   = " \n\t.,:;!\"\'()<>/\\"
 
-
+-- lookUp takes a search string and a list of string/item pairs as arguments 
+-- and outputs the list of items for which the specific string matches the
+-- search string 
 lookUp :: String -> [(String, a)] -> [a]
 lookUp  str list
   = [x | (str', x) <- list, str == str']  
 
+-- split operates recursively to break up a string given a list of separators 
+-- within the string and returns a tuple with the separators and a list of the
+-- words in the string (with "" in the place of separators within the list of
+-- words to show where they go)
 split :: [Char] -> String -> (String, [String])
 split  _ ""
   = ("", [""])
@@ -26,11 +32,22 @@ split seps (y : ys)
     where
       (seps',(x : xs)) = split seps ys
 
+-- combine performs the opposing action to split in that, when given a string of
+-- separators and a list of strings it concatenates them into a single list of
+-- strings
 combine :: String -> [String] -> [String]
-combine = error "TODO: implement combine"
+combine "" string 
+  = string
+combine (sep : seps) (x : xs)
+  = x : [sep] : combine seps xs
 
 getKeywordDefs :: [String] -> KeywordDefs
-getKeywordDefs = error "TODO: implement getKeywordDefs"
+getKeywordDefs []
+  = []
+getKeywordDefs (x : xs)
+  = (keyw, concat (combine spaces def)) : getKeywordDefs xs
+    where 
+      (_ : spaces, keyw : def )= split " " x 
 
 expand :: FileContents -> FileContents -> FileContents
 expand = error "TODO: implement expand"
